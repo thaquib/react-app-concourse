e33a1e2b0abbc71623f774032e2d36e346ea8233
Ahammed, Thaquib <Thaquib_Ahammed@comcast.com>
2022-07-28 14:34:56 -0400

    new dockerfile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          thaquib is cool
+          thaquib is cool. poop
         </p>
         <a
           className="App-link"
